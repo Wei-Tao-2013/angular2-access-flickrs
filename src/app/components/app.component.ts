05513d21497845c3ec5cdeadc5fb83b07f3e702e
kevintaowei <kevin.taowei@gmail.com>
2017-11-23 10:57:33 +1100

    Add files via upload

---
+++ src/app/components/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit} from '@angular/core';
+import { FlickrService } from '../services/flickr.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+//import 'rxjs/Rx';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  providers: [FlickrService]
+})
+export class MainAppComponent implements OnInit {
+  searchControl = new FormControl();
+  model$: Observable<any>;
+  flickrs: Object;
+  constructor(private _formBuilder: FormBuilder, private _flickrService: FlickrService) {
+  }
+ 
+  ngOnInit() {
+      this.searchControl.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .switchMap((query: string) => this._flickrService.getResult(query))
+      .subscribe(value => {
+        this.flickrs = value;
+      });
+  }
+  
+}
